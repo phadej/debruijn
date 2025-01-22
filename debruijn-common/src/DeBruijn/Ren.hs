bfe8848a07f0e0ec1a1615114e3689c6cfea9d4f
Oleg Grenrus <oleg.grenrus@iki.fi>
2025-01-22 13:53:42 +0200

    Remove IdxMapping, add Contract

--- debruijn-common/src/DeBruijn/Ren.hs
+++ debruijn-common/src/DeBruijn/Ren.hs
@@ -10,24 +10,18 @@ module DeBruijn.Ren (
     -- ** Category
     idRen,
     compRen,
-    -- * Applicative renamings
-    RenA (..),
-    renameIdxA,
-    keepRenA,
+    -- * Contractions
     unusedIdx,
     -- * Renameble things
-    IdxMapping (..),
+    Keep (..),
     keepAdd,
     Weaken (..),
+    Contract (..),
     Rename (..),
-    RenameA (..),
-    defaultRename,
-    defaultWeaken,
 ) where
 
-import Data.Functor.Identity (Identity (..))
-import Data.Kind             (Constraint, Type)
-import Data.Proxy            (Proxy (..))
+import Data.Kind  (Constraint, Type)
+import Data.Proxy (Proxy (..))
 
 import DeBruijn.Add
 import DeBruijn.Ctx
@@ -35,8 +29,6 @@ import DeBruijn.Env
 import DeBruijn.Idx
 import DeBruijn.Size
 import DeBruijn.Wk
-
-import TrustworthyCompat (coerce)
 
 -------------------------------------------------------------------------------
 -- Renamings
@@ -88,39 +80,22 @@ absurdRen = MkRen EmptyEnv
 -- Applicative renamings
 -------------------------------------------------------------------------------
 
-type RenA :: (Type -> Type) -> Ctx -> Ctx -> Type
-newtype RenA f ctx ctx' = MkRenA (Env ctx (f (Idx ctx')))
-
--- | Lift renaming (used when going under a binder).
-keepRenA :: Applicative f => RenA f ctx ctx' -> RenA f (S ctx) (S ctx')
-keepRenA (MkRenA js) = MkRenA (fmap (fmap IS) js :> pure IZ)
-
-unusedIdx :: Size ctx -> RenA Maybe (S ctx) ctx
-unusedIdx s = MkRenA $ tabulateEnv (SS s) $ unIdx Nothing Just
+unusedIdx :: Wk ctx (S ctx)
+unusedIdx = wk1
 
 -------------------------------------------------------------------------------
 -- Renameble & RenamebleA
 -------------------------------------------------------------------------------
 
--- | 'IdxMapping' generalizes over various index mappings, also effectful ones.
-type IdxMapping :: (Type -> Type) -> (Ctx -> Ctx -> Type) -> Constraint
-class Applicative f => IdxMapping f t | t -> f where
-    -- | 'IdxMapping' action.
-    mapIdx :: t ctx ctx' -> Idx ctx -> f (Idx ctx')
-
+type Keep :: (Ctx -> Ctx -> Type) -> Constraint
+class Keep t where
     -- | 'keep' is often called @lift@, but we stick with 'Wk' terminology.
     -- One benefit is that the name 'keep' is less likely to clash.
     keep   :: t ctx ctx' -> t (S ctx) (S ctx')
 
-    -- | Compose weakening with an index mapping.
-    --
-    -- This is useful when you have explicit weakening in your terms.
-    -- (a similar idea as in @bound@'s @Scope@ possibly lifting whole term).
-    weakenIdxMapping :: Wk ctx ctx' -> t ctx' ctx'' -> t ctx ctx''
-
 -- | 'keep' 'IdxMapping' @arity@ times.
 keepAdd
-    :: IdxMapping f m
+    :: Keep m
     => Add arity ctxA ctxA'
     -> m ctxA ctxB
     -> (forall ctxB'. Add arity ctxB ctxB' -> m ctxA' ctxB' -> r)
@@ -128,23 +103,11 @@ keepAdd
 keepAdd AZ     w kont = kont AZ w
 keepAdd (AS a) w kont = keepAdd a w $ \a' w' -> kont (AS a') (keep w')
 
-instance IdxMapping Identity Wk where
+instance Keep Wk where
     keep = KeepWk
-    mapIdx w x = Identity (weakenIdx w x)
-    weakenIdxMapping = compWk
 
-instance IdxMapping Identity Ren where
+instance Keep Ren where
     keep = keepRen
-    mapIdx w x = Identity (renameIdx w x)
-    weakenIdxMapping w (MkRen is) = MkRen (weakenEnv w is)
-
-instance Applicative f => IdxMapping f (RenA f) where
-    keep = keepRenA
-    mapIdx = renameIdxA
-    weakenIdxMapping w (MkRenA is) = MkRenA (weakenEnv w is)
-
-renameIdxA :: RenA f ctx ctx' -> Idx ctx -> f (Idx ctx')
-renameIdxA (MkRenA js) i = lookupEnv i js
 
 -- | Renameble things.
 --
@@ -161,28 +124,9 @@ class Weaken t where
 class Weaken t => Rename t where
     rename :: Ren n m -> t n -> t m
 
--- | 'rename' implementation using 'grename'.
-defaultRename :: forall t n m. RenameA t => Ren n m -> t n -> t m
-defaultRename = coerce (grename @t @Ren @Identity @n @m)
-
--- | 'weaken' implementation using 'grename'.
-defaultWeaken :: forall t n m. RenameA t => Wk n m -> t n -> t m
-defaultWeaken = coerce (grename @t @Wk @Identity @n @m)
-
--- | Effectful renamings.
---
--- An common example is checking whether a binding is used:
---
--- @
--- Just t' <- 'renameA' 'unusedIdx' t
--- @
---
-class Rename t => RenameA t where
-    renameA :: forall f ctx ctx'. Applicative f => RenA f ctx ctx' -> t ctx -> f (t ctx')
-    renameA = grename
-
-    -- | Generic renaming of a term @t@ using any 'IdxMapping'.
-    grename :: forall m f ctx ctx'. IdxMapping f m => m ctx ctx' -> t ctx -> f (t ctx')
+-- | Contractions
+class Contract t where
+    contract :: Wk m n -> t n -> Maybe (t m)
 
 instance Weaken Proxy where
      weaken _ _ = Proxy
@@ -190,8 +134,8 @@ instance Weaken Proxy where
 instance Rename Proxy where
     rename _ _ = Proxy
 
-instance RenameA Proxy where
-    grename _ _ = pure Proxy
+instance Contract Proxy where
+    contract _ _ = Just Proxy
 
 instance Weaken Idx where
     weaken = weakenIdx
@@ -199,8 +143,8 @@ instance Weaken Idx where
 instance Rename Idx where
     rename = renameIdx
 
-instance RenameA Idx where
-    grename = mapIdx
+instance Contract Idx where
+    contract = contractIdx
 
 instance Weaken (Ren n) where
     weaken w (MkRen js) = MkRen (fmap (weaken w) js)
